53b66aeec3468e46080cc6dc8ca7ceb12204f5d9
evellynCaroline <evellynopo@gmail.com>
2023-06-12 10:39:25 -0400

    DGV

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DataGridViewdoiss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataTable = new System.Windows.Forms.DataGridView();
+            this.LblNomeGrid = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataTable
+            // 
+            this.DataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataTable.Location = new System.Drawing.Point(87, 146);
+            this.DataTable.Name = "DataTable";
+            this.DataTable.RowHeadersWidth = 51;
+            this.DataTable.RowTemplate.Height = 24;
+            this.DataTable.Size = new System.Drawing.Size(600, 281);
+            this.DataTable.TabIndex = 0;
+            // 
+            // LblNomeGrid
+            // 
+            this.LblNomeGrid.AutoSize = true;
+            this.LblNomeGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNomeGrid.Location = new System.Drawing.Point(274, 47);
+            this.LblNomeGrid.Name = "LblNomeGrid";
+            this.LblNomeGrid.Size = new System.Drawing.Size(241, 32);
+            this.LblNomeGrid.TabIndex = 1;
+            this.LblNomeGrid.Text = "Dados dos alunos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 455);
+            this.Controls.Add(this.LblNomeGrid);
+            this.Controls.Add(this.DataTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataTable;
+        private System.Windows.Forms.Label LblNomeGrid;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewdoiss
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarDados();
+        }
+
+        private void CarregarDados()
+        {
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Nome", typeof(string));
+            dt.Columns.Add("idade", typeof(int));
+            dt.Columns.Add("Email", typeof (string));
+
+            dt.Rows.Add(new object[] {01 , "Amanda Cores", 16, "juu@gmail.com"});
+            dt.Rows.Add(new object[] {02, "Andre Coelho", 16, "Andre@gmail.com"  });
+            dt.Rows.Add(new object[] {03, "Cassia Jesus", 17, "Cassinha@gmail.com" });
+            dt.Rows.Add(new object[] {04, "Carla Silva", 16, "Carla@gmail.com" });
+            dt.Rows.Add(new object[] {05, "Davi Roma", 16, "Davi@gmail.com" });
+            dt.Rows.Add(new object[] {06, "Daniela Tamires", 17, "Dani@gmail.com" });
+
+
+
+
+            DataTable.DataSource = dt;
+        }
+    }
+}
